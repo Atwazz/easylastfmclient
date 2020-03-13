b80850048c6b0098c26332d5ac9be98bf05e3a2b
Artem Agafonov <atwazz@mail.ru>
2020-03-14 01:36:21 +0400

    Initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform: ios, '13.0'
+use_modular_headers!
+target 'Test App Artem Agafonov' do
+  pod 'AlamofireImage', '~> 4.0'
+  pod 'Alamofire', '~> 5.0'
+  pod 'SwiftLint'
+end
---
+++ Test App Artem Agafonov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test App Artem Agafonov
+//
+//  Created by Artem Agafonov on 14.03.2020.
+//  Copyright © 2020 Artem Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
