1b5c6a9ff52f547496d2590bf52ffb3e3e57f9e0
Artem Agafonov <atwazz@mail.ru>
2021-04-19 12:24:10 +0300

    Improved ThreadSafe property wrapper.

--- EasyLastFMClient/Common/PropertyWrappers/ThreadSafe.swift
+++ EasyLastFMClient/Common/PropertyWrappers/ThreadSafe.swift
@@ -10,42 +10,44 @@ import Foundation
 
 @propertyWrapper
 final class ThreadSafe<Value> {
-    private var _value: Value?
-    private let _queue: DispatchQueue
-    
-    init() {
-        _value = nil
-        _queue = Self.defaultQueue
-    }
-    
-    init(wrappedValue value: Value?, queue: DispatchQueue? = nil) {
-        _value = value
-        guard let queue = queue else {
-            _queue = Self.defaultQueue
-            return
-        }
-        _queue = queue
-    }
-    
-    var wrappedValue: Value? {
-        get {
-            return _queue.sync { [weak self] in
-                return self?._value
-            }
-        }
-        set {
-            _queue.async(flags: .barrier) { [weak self] in
-                self?._value = newValue
-            }
-        }
-    }
+	// MARK: - Private instance properties
+	private var _value: Value
+	private let _queue: DispatchQueue
+
+	// MARK: - Init
+	init(wrappedValue value: Value, queue: DispatchQueue? = nil) {
+		_value = value
+		_queue = queue ?? Self.defaultQueue
+	}
+
+	var wrappedValue: Value {
+		get {
+			 _queue.sync {
+				return self._value
+			}
+		}
+		set {
+			_queue.async(flags: .barrier) { [weak self] in
+				self?._value = newValue
+			}
+		}
+	}
+}
+
+// MARK: - Public
+extension ThreadSafe {
+	func mutate(_ mutation: (inout Value) -> Void) {
+		_queue.sync {
+			mutation(&_value)
+		}
+	}
 }
 
 // MARK: - Private
 private extension ThreadSafe {
-    static var defaultQueue: DispatchQueue {
-        DispatchQueue(label: UUID().uuidString,
-                      qos: .userInteractive,
-                      attributes: .concurrent)
-    }
+	static var defaultQueue: DispatchQueue {
+		DispatchQueue(label: UUID().uuidString,
+					  qos: .userInteractive,
+					  attributes: .concurrent)
+	}
 }
